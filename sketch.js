56db7b09ea8968c1f83b3e842c93826b83d5db3a
Melissa Martinez <melmar12@live.com>
2017-06-08 18:35:09 -0500

    Serpinski Triangle

--- sketch.js
+++ sketch.js
@@ -1,9 +1,24 @@
+function drawCircle(x,y,d) {
+  stroke(0);
+  noFill();
+  ellipse(x,y,d,d);
+  if (d > 3) {
+    drawCircle(x-d/2, y, d/2);
+    drawCircle(x+d/2, y, d/2);
+    drawCircle(x, y+d/2, d/2);
+  }
+}
 
-function setUp() {
-
+function setup() {
+    createCanvas(windowWidth, windowHeight);
 
 }
 
 function draw() {
-  
+  background(255);
+  drawCircle(width/2, height/2, 300);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
